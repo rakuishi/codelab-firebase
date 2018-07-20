714243cc6aff44edb9e51f67f06889f1fe032030
rakuishi <rakuishi@gmail.com>
2018-07-20 23:27:44 +0900

    Show Notifications

--- scripts/main.js
+++ scripts/main.js
@@ -104,11 +104,28 @@ function saveImageMessage(file) {
 // Saves the messaging device token to the datastore.
 function saveMessagingDeviceToken() {
   // TODO 10: Save the device token in the realtime datastore
+  firebase.messaging().getToken().then(function(currentToken) {
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      firebase.database().ref('/fcmTokens').child(currentToken)
+        .set(firebase.auth().currentUser.uid);
+    } else {
+      requestNotificationsPermissions();
+    }
+  }).catch(function(error) {
+    console.error('Unable to get messaging token.', error);
+  });
 }
 
 // Requests permissions to show notifications.
 function requestNotificationsPermissions() {
   // TODO 11: Request permissions to send notifications.
+  console.log('Requesting notifications permission...');
+  firebase.messaging().requestPermission().then(function() {
+    saveMessagingDeviceToken();
+  }).catch(function(error) {
+    console.error('Unable to get permission to notify.', error);
+  });
 }
 
 // Triggered when a file is selected via the media picker.
